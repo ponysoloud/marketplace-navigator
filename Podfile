aaac9e2fe9bffdae583a8d53115f053ed32a1b5e
Александр Пономарев <alex.ponomarev@MacBook-Pro-Aleksandr.local>
2017-07-28 18:45:40 +0600

    Add Alamofire through the Cocoapods

---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target ‘marketplace_navigator’ do
+    pod 'Alamofire', '~> 4.4'
+end
---
+++ marketplace_navigator/DataManager.swift
@@ -0,0 +1,9 @@
+//
+//  DataManager.swift
+//  marketplace_navigator
+//
+//  Created by Александр Пономарев on 28.07.17.
+//  Copyright © 2017 Base team. All rights reserved.
+//
+
+import Foundation
