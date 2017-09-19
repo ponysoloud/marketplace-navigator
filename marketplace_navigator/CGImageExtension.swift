17e1d937ade097eaaaed33d1ef866db37d30e27f
Александр Пономарев <alex.ponomarev@MacBook-Pro-Aleksandr.local>
2017-09-19 20:54:06 +0300

    Significant design changes

---
+++ marketplace_navigator/CGImageExtension.swift
@@ -0,0 +1,9 @@
+//
+//  CGImageExtension.swift
+//  marketplace_navigator
+//
+//  Created by Александр Пономарев on 18.09.17.
+//  Copyright © 2017 Base team. All rights reserved.
+//
+
+import Foundation
---
+++ marketplace_navigator/UIViewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewExtension.swift
+//  marketplace_navigator
+//
+//  Created by Александр Пономарев on 18.09.17.
+//  Copyright © 2017 Base team. All rights reserved.
+//
+
+import Foundation
